1a1f4a372684e721cd99a18f46715ec7040debd3
Agnivesh Patel <agnivesh@monosoz.com>
2016-08-29 22:10:44 +0530

    op

--- resources/views/orders_o.blade.php
+++ resources/views/orders_o.blade.php
@@ -26,10 +26,19 @@
             @if (count($orders) === 0)
             <p>Nothing to show here!</p>
             @else
+
+{{--*/ $opcal = new stdClass() /*--}}
+{{--*/ $opcal->total = 0 /*--}}
+{{--*/ $opcal->off = 0 /*--}}
+{{--*/ $opcal->ocount = 0 /*--}}
+{{--*/ $opcal->pcount = 0 /*--}}
+
+
                 @foreach ($orders->sortByDesc('updated_at') as $cart)
 <div>
 Order Id: {{$cart->id}}<br>
 User: {{$cart->user->name}} <a href="?u={{$cart->user->id}}">({{$cart->user->id}})</a><br>
+User Contact: {{$cart->user->mobile_number}}<br>
 @if ($cart->is('pending'))
 Pending
 @else
@@ -54,6 +63,10 @@ Order Status: {{$cart->statusCode}}
 <br>
 {{$cart->created_at}}<br>
 Total: {{$cart->total}}<br>
+
+{{--*/ $opcal->ocount++ /*--}}
+{{--*/ $opcal->total += $cart->total /*--}}
+
 <div style="text-align: center;">
 <strong>monosoz</strong><br>
 New Delhi - 110017<br>
@@ -130,6 +143,7 @@ Address:{{$cart->delivery_location->address}}<br>
         <td><span>
             @if(substr($item->class, 0, 4)==='App\\')
             {{ $item->displayName }}
+{{--*/ $opcal->pcount++ /*--}}
             @elseif(substr($item->sku, 0, 4)==='FREE')
             Free Pizza
             @else
@@ -141,6 +155,7 @@ Address:{{$cart->delivery_location->address}}<br>
                 @elseif(substr($item->sku, -1)==='L')
                 -Large
                 @endif
+{{--*/ $opcal->pcount++ /*--}}
             @endif
         </span></td>
         <td><i class="fa fa-inr"></i><span> {{ $item->price + 0 }}</span>
@@ -190,6 +205,12 @@ www.monosoz.com<br>
       </div>
     </div>
   </div>
+</div>
+<div style="padding: 31px; position: fixed; left: 0; right: 0; bottom: 0; background-color: #eee; z-index: 1;">
+<span class="col-sm-3 col-xs-6">Total: {{ $opcal->total }}</span>
+<span class="col-sm-3 col-xs-6">Discount: {{ $opcal->off }}</span>
+<span class="col-sm-3 col-xs-6">Order Count: {{ $opcal->ocount }}</span>
+<span class="col-sm-3 col-xs-6">Pizza Count: {{ $opcal->pcount }}</span>
 </div>
 @endsection
 
